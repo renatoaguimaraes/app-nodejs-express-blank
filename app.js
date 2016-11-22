3fc5689619c1e53d0f316725db5df5323ffc2289
Renato Guimarães <renatoaguimaraes@gmail.com>
2016-11-22 08:37:05 -0200

    Update app.js

--- app.js
+++ app.js
@@ -3,17 +3,13 @@ var app = express();
 var http = require('http');
 var bodyParser = require('body-parser');
 
-//Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-//View engine setup
 app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/public');
 app.engine('html', require('ejs').renderFile);
 
-
-//Server Listening
 var standardServer = http.createServer(app);
 standardServer.listen(process.env.PORT || 5000,function (){
     console.log('HTTP Server on');
